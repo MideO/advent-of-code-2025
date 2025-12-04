15c646cbbd9bf691887d7a4466f044f13e9e4f79
Mide Ojikutu <olumide.ojikutu@sky.uk>
2025-12-04 10:47:25 +0000

    Day Two Part One

--- exercises/src/main/java/com/github/mideo/exercises/DayTwo.java
+++ exercises/src/main/java/com/github/mideo/exercises/DayTwo.java
@@ -1,19 +1,65 @@
 package com.github.mideo.exercises;
 
+import com.github.mideo.InputReader;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 @DailyExercise
-public class DayTwo implements Exercise<Integer, Integer>{
-  @Override
-  public String name() {
-    return "Day 2: Gift Shop";
-  }
+public class DayTwo implements Exercise<Long, Long> {
+    @Override
+    public String name() {
+        return "Day 2: Gift Shop";
+    }
 
-  @Override
-  public Integer partOne() {
-    return 0;
-  }
+    static LongStream stream(String range) {
+        var values = Arrays.stream(range.split("-")).mapToLong(Long::parseLong).toArray();
+        return LongStream.rangeClosed(values[0], values[values.length - 1]);
+    }
 
-  @Override
-  public Integer partTwo() {
-    return 0;
-  }
+    record Id(String value) {
+
+        boolean isSequenceRepeated(int times) {
+            if (value.length() % times != 0) return false;
+            int half = value.length() / times;
+            var subsstring = value.substring(0, half);
+            return subsstring.repeat(times).equals(value);
+
+        }
+    }
+
+
+    record Ids(String[] values) {
+
+        Stream<Id> getIdsWithRepeatedSequence(int numberOfRepeatedSequence) {
+            return Arrays.stream(values)
+                    .flatMap(
+                            range -> {
+                                var numbers = stream(range);
+                                return numbers.mapToObj(number -> new Id(String.valueOf(number)))
+                                        .filter(id -> id.isSequenceRepeated(numberOfRepeatedSequence));
+                            }
+                    );
+
+
+        }
+    }
+
+    @Override
+    public Long partOne() {
+
+        return InputReader
+                .readLines("DayTwoInput.txt")
+                .stream()
+                .map(input -> new Ids(input.split(",")))
+                .flatMap(ids -> ids.getIdsWithRepeatedSequence(2))
+                .mapToLong(id -> Integer.parseInt(id.value))
+                .sum();
+    }
+
+    @Override
+    public Long partTwo() {
+        return 0L;
+    }
 }
---
+++ exercises/src/test/java/com/github/mideo/exercises/DayTwoTest.java
@@ -0,0 +1,76 @@
+package com.github.mideo.exercises;
+
+
+import com.github.mideo.InputReader;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+import static com.github.mideo.exercises.DayTwo.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayTwoTest {
+
+    @Test
+    void getRangeFromNumbersRange() {
+        var expected = LongStream.rangeClosed(11L, 22L);
+        var actual = stream("11-22");
+        assertIterableEquals(expected.boxed().toList(), actual.boxed().toList());
+    }
+
+    @Test
+    void idIsNotSequenceRepeated() {
+        assertFalse(new Id("1").isSequenceRepeated(2));
+        assertFalse(new Id("19").isSequenceRepeated(2));
+        assertFalse(new Id("9191896883").isSequenceRepeated(2));
+        assertFalse(new Id("9199").isSequenceRepeated(2));
+    }
+
+    @Test
+    void idIsSequenceRepeated() {
+        assertTrue(new Id("11").isSequenceRepeated(2));
+        assertTrue(new Id("1010").isSequenceRepeated(2));
+        assertTrue(new Id("1188511885").isSequenceRepeated(2));
+        assertTrue(new Id("222222").isSequenceRepeated(2));
+        assertTrue(new Id("38593859").isSequenceRepeated(2));
+
+    }
+
+    @Test
+    void returnsInvalidIds(){
+        var input = """
+                11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+                1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+                824824821-824824827,2121212118-2121212124
+                """.replaceAll("\\s", "");
+
+        assertIterableEquals(
+                List.of(
+                        new Id("11"),
+                        new Id("22"),
+                        new Id("99"),
+                        new Id("1010"),
+                        new Id("1188511885"),
+                        new Id("222222"),
+                        new Id("446446"),
+                        new Id("38593859")
+                        ),
+                new Ids(input.split(",")).getIdsWithRepeatedSequence(2).toList()
+        );
+    }
+    @Test
+    void name() {
+        assertEquals("Day 2: Gift Shop", new DayTwo().name());
+    }
+
+    @Test
+    void partOne() {
+        assertEquals(5398419778L, new DayTwo().partOne());
+    }
+
+    @Test
+    void partTwo() {
+    }
+}
