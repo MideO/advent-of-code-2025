4f787f0ba344d537c9de4c168220251282a7e839
Mide Ojikutu <mide.ojikutu@gmail.com>
2025-12-04 06:06:12 +0000

    WIP Day Two Part One

---
+++ exercises/src/main/java/com/github/mideo/exercises/DayTwo.java
@@ -0,0 +1,19 @@
+package com.github.mideo.exercises;
+
+@DailyExercise
+public class DayTwo implements Exercise<Integer, Integer>{
+  @Override
+  public String name() {
+    return "Day 2: Gift Shop";
+  }
+
+  @Override
+  public Integer partOne() {
+    return 0;
+  }
+
+  @Override
+  public Integer partTwo() {
+    return 0;
+  }
+}
